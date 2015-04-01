22de224e0a1c5a44f86f73735686820e8a68ab99
Jordan Collier <jordanicollier@gmail.com>
2015-03-31 21:26:21 -0600

    can update and refresh what's up

--- app/assets/javascripts/edit_status.js
+++ app/assets/javascripts/edit_status.js
@@ -7,27 +7,38 @@
   }
 
   WhatsUpListener.prototype.addObservers = function() {
+    var _this = this;
     this.$el.click(function(e) {
       e.preventDefault();
       $(this).siblings('.status').hide();
       $(this).siblings('form').find('input').show();
-      $('#user_form').submit(function(e) {
-        e.preventDefault();
-        var whatsUp = $('#user_form input').val();
-        var userId  = window.location.href.match(/users\/()\d/)[1];
-
-        $.ajax('/users/'+userId, {
-          dataType: 'json',
-          type: 'put',
-          data: {
-            id: userId,
-            users: { "whats_up": whatsUp }
-          },
-          success: function() { console.log('lol yes'); },
-          error: function() { console.log('luke no'); }
-        })
-      })
+      $('#user_form').submit(_this.addUpdateUser.bind(_this));
     });
+  }
+
+  function userId() {
+    return window.location.href.match(/users\/(\d)/)[1];
+  }
+
+  WhatsUpListener.prototype.addUpdateUser = function(event) {
+    event.preventDefault();
+    var whatsUp = $('#user_form input').val();
+
+    $.ajax('/users/'+userId(), {
+      dataType: 'json',
+      type: 'put',
+      data: {
+        id: userId(),
+        user: { "whats_up": whatsUp }
+      },
+      success: this.showUpdateStatus.bind(this, whatsUp),
+      error: function() { console.log('luke no'); }
+    })
+  }
+
+  WhatsUpListener.prototype.showUpdateStatus = function(newValue) {
+    this.$el.siblings('form').hide();
+    this.$el.siblings('.status').text(newValue).fadeIn();
   }
 
 
