d97f1239292b42a4fef424bc0d008232b73c0392
Jordan Collier <jordanicollier@gmail.com>
2015-04-03 15:37:22 -0600

    make seed file for game population

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Game::SMASH_GAMES.each do |name, platform|
+  Game.create(name: name, platform: platform)
+end
