7996796306a1f5b18923654fbfadd2ddd0db7980
Jordan Collier <jordanicollier@gmail.com>
2015-04-03 15:38:31 -0600

    migrations for join table and game table

---
+++ db/migrate/20150403005914_create_game_users.rb
@@ -0,0 +1,9 @@
+class CreateGameUsers < ActiveRecord::Migration
+  def change
+    create_table :game_users do |t|
+      t.integer :game_id
+      t.integer :user_id
+      t.timestamps null: false
+    end
+  end
+end
