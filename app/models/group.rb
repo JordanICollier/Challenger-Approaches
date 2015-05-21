2fb8ea58212eff5b8ab6335af9480529e9d6d86b
JordanCollier <jordanicollier@gmail.com>
2015-05-21 10:53:32 -0600

    add group model with setup

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  belongs_to :location
+  has_one :location
   belongs_to :host, foreign_key: :user_id
   has_many :members, class_name: "User", table_name: 'users'
 
