9c7f7d54ef232eecc3602d4bc4951099073b7842
JordanCollier <jordanicollier@gmail.com>
2015-05-21 10:52:24 -0600

    add group, and location routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :dashboard
+  resources :dashboard, :maps
+  resources :locations, only: %i(create)
+  resources :groups
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
