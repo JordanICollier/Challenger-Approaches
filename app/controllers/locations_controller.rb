fe2c78cab117a234f4f3e2abfec447562c54e9bc
JordanCollier <jordanicollier@gmail.com>
2015-05-21 10:56:17 -0600

    alex-style params to locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,7 +6,7 @@ class LocationsController < ApplicationController
   private
 
   def location_params
-    params.require(:location).permit(:lat, :long, :user_id, :formatted_address).tap { |u|
+    params.require(:location).permit(:lat, :long, :user_id, :formatted_address, :group_id).tap { |u|
       u[:user_id] = current_user.id
     }
   end
