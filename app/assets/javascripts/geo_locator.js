541ad8cf544e321474e1e763975129e5e143cdb9
JordanCollier <jordanicollier@gmail.com>
2015-05-21 11:01:27 -0600

    geo_locator can now set address from api

--- app/assets/javascripts/geo_locator.js
+++ app/assets/javascripts/geo_locator.js
@@ -1,38 +1,67 @@
 (function() {
 
   var GeoLocator = {
-    addObservers: function() {
-      $('[type="submit"]').on('click', this.fetchCoordinates);
-    },
-    fetchCoordinates: function(e) {
-      e.preventDefault();
 
-      var userLocation = $('.user-location').val();
+    addObservers: function() {
+      $('.geo-form').on('click', '[type="submit"]', this.submitForm);
+    },
+
+    fetchCoordinates: function(json) {
+
+      var userLocation = $('[class$="location"]').val();
 
       var geocoder = new google.maps.Geocoder();
 
       geocoder.geocode({ address: userLocation }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-          GeoLocator.createLocation(results[0]);
+          GeoLocator.createLocation(results[0], json);
         } else {
           console.log('failed hard', status, results);
-        };
+        }
+      });
+
+    },
+
+    submitForm: function(e) {
+      e.preventDefault();
+
+      $.ajax({
+        url: '/groups',
+        method: 'POST',
+        type: 'JSON',
+        processData: false,
+        contentType: false,
+        data: new FormData($('.geo-form')[0]),
+        success: GeoLocator.fetchCoordinates
       });
     },
-    createLocation: function(json) {
+
+    createLocation: function(json, groupJson) {
+      debugger
       $.ajax({
         url: '/locations',
         method: 'POST',
         type: 'JSON',
         data: {
           location: {
-            user_id: SmashBros.WhatsUpListener.prototype.userId.call(),
             lat: json.geometry.location.A,
-            long: json.geometry.location.F
-            formatted_name: json.formatted_address
+            long: json.geometry.location.F,
+            formatted_address: json.formatted_address,
+            group_id: groupJson.id
           }
-        }
-      })
+        },
+        success: GeoLocator.redirectToUser
+      });
+    },
+
+    supportedRedirects: {
+      users: function() { return "/users/" + SmashBros.WhatsUpListener.prototype.userId.call(); },
+      groups: function() { return "/groups"; }
+    },
+
+    redirectToUser: function() {
+      var type = window.location.href.match(/\/(\w+)\//)[1];
+      window.location = GeoLocator.supportedRedirects[type].call();
     }
   };
 
