cdd3f03739e12a7bc1ae9d63aed869cfcee17800
JordanCollier <jordanicollier@gmail.com>
2015-05-21 10:49:50 -0600

    add locations to groups

--- db/migrate/20150502152519_add_location_to_group.rb
+++ db/migrate/20150502152519_add_location_to_group.rb
@@ -1,8 +1,9 @@
 class AddLocationToGroup < ActiveRecord::Migration
   def change
     change_table :locations do |t|
-      t.belongs_to :group
       t.string :formatted_address
+      t.belongs_to :group
     end
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,11 +49,11 @@ ActiveRecord::Schema.define(version: 20150502152519) do
   create_table "locations", force: :cascade do |t|
     t.decimal  "lat",               precision: 10, scale: 6
     t.decimal  "long",              precision: 10, scale: 6
-    t.integer  "user_id"
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
-    t.integer  "group_id"
+    t.integer  "user_id"
     t.string   "formatted_address"
+    t.integer  "group_id"
   end
 
   create_table "users", force: :cascade do |t|
