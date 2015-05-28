51b868c57775d18105fab7336c1164823f1c0b34
JordanCollier <jordanicollier@gmail.com>
2015-05-28 00:03:39 -0600

    add default image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,6 +16,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url
+    "http://www.ssbwiki.com/images/f/fc/SmashBrosSymbol.png"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
