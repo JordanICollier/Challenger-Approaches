5868acc442998d615344ecd3a88ccec697930f07
JordanCollier <jordanicollier@gmail.com>
2015-06-10 12:34:39 -0600

    add users within group to display on group show page

--- db/schema.rb
+++ db/schema.rb
@@ -59,9 +59,9 @@ ActiveRecord::Schema.define(version: 20150525152957) do
   create_table "locations", force: :cascade do |t|
     t.decimal  "lat",               precision: 10, scale: 6
     t.decimal  "long",              precision: 10, scale: 6
+    t.integer  "user_id"
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
-    t.integer  "user_id"
     t.string   "formatted_address"
     t.integer  "group_id"
   end
